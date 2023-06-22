84f700148aff999aedac755efdbe3eb02f35306e
VISTAJET\asingh <akash.singh@vistaglobal.com>
2023-06-22 20:59:53 +0530

    test_commit

--- src/main/java/dsalgo/learning/leetcode/Playground.java
+++ src/main/java/dsalgo/learning/leetcode/Playground.java
@@ -1,16 +1,10 @@
 package dsalgo.learning.leetcode;
 
+// Normal playground file for testing
 public class Playground {
     public static void main(String[] args) {
 
         int[][] test = {{1, 2}, {3, 4}};
-
-//        method(test);
-//
-//        for (int i = 0; i < 2; i++) {
-//            for (int j = 0; j < 2; j++)
-//                System.out.print(test[i][j] + " ");
-//        }
 
         System.out.println(3 ^ 5 ^ 7 ^ 2 ^ 3 ^ 2);
     }
